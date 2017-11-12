49355caf746992ccd03b9a98c0f3673a182c34c2
wbh <1762629204@qq.com>
2017-11-12 11:49:40 +0800

    test

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:123@localhost:3306/board?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:wbh673406316@localhost:3306/board?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = 'hard to guess in the future'
-    ADMIN = 'ADMIN'
+    ADMIN = '我'
